7fc5bb62fc596ef7875ab148bb46b56dd869c871
Yorkys <yyorkyss@gmail.com>
2024-09-05 23:35:04 +0200

    Added the InventoryManager and the MenuListener to handle the Menu/Inventory system

---
+++ src/main/java/com/yorkys/plugintest/Inventory/InventoryManager.java
@@ -0,0 +1,36 @@
+package com.yorkys.plugintest.Inventory;
+
+import com.yorkys.plugintest.Inventory.menus.SettingsMenu;
+import com.yorkys.plugintest.MiniGame;
+import lombok.Getter;
+import org.bukkit.entity.Player;
+import java.util.HashMap;
+import java.util.Map;
+
+@Getter
+public class InventoryManager {
+
+    private final MiniGame miniGame;
+    private static final Map<Integer, Menu> openedInventory = new HashMap<>();
+
+    public InventoryManager(MiniGame miniGame) {
+        this.miniGame = miniGame;
+    }
+
+    public void openSettingsMenu(Player player) {
+        new SettingsMenu(miniGame, player).open(player.getPlayer());
+    }
+
+    public Menu getOpened(Player player) {
+        return openedInventory.get(player.getEntityId());
+    }
+
+    public void closeMenu(Player player) {
+        openedInventory.remove(player.getEntityId());
+    }
+
+    public void addOpenedInventory(int entityId, Menu menu) {
+        openedInventory.put(entityId, menu);
+    }
+
+}
--- src/main/java/com/yorkys/plugintest/listeners/MenuListener.java
+++ src/main/java/com/yorkys/plugintest/listeners/MenuListener.java
@@ -1,12 +1,16 @@
 package com.yorkys.plugintest.listeners;
 
 
+import com.yorkys.plugintest.Inventory.Menu;
 import com.yorkys.plugintest.MiniGame;
 import lombok.RequiredArgsConstructor;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 
@@ -15,14 +19,32 @@ public class MenuListener implements Listener {
 
     private final MiniGame miniGame;
 
+    @EventHandler
     public void PlayerInteractEvent(PlayerInteractEvent event) {
         ItemStack comparatorItem = new ItemStack(Material.REDSTONE_COMPARATOR, 1);
         Player player = event.getPlayer();
 
         if (player.getInventory().getItemInHand().isSimilar(comparatorItem)) {
             if (event.getAction().equals(Action.RIGHT_CLICK_BLOCK) || event.getAction().equals(Action.RIGHT_CLICK_AIR)) {
-                //open menu
+                miniGame.getInventoryManager().openSettingsMenu(event.getPlayer());
+                event.setCancelled(true);
             }
         }
+    }
+
+    @EventHandler
+    public void onMenuClick(InventoryClickEvent event) {
+        Menu menu = miniGame.getInventoryManager().getOpened((Player) event.getWhoClicked());
+        if (menu == null) return;
+        switch (event.getAction()) {
+            case PICKUP_ALL, PICKUP_HALF, MOVE_TO_OTHER_INVENTORY -> menu.computeIfPresent(event.getSlot(), event);
+        }
+
+        event.setCancelled(true);
+    }
+
+    @EventHandler
+    public void onCloseMenu(InventoryCloseEvent event) {
+        miniGame.getInventoryManager().closeMenu((Player) event.getPlayer());
     }
 }
