b6fad3cc83fd90b652ed3231917c147b849ab6d7
Yorkys <yyorkyss@gmail.com>
2024-09-08 22:22:41 +0200

    Added config manager to handle all the config (.yml) files

---
+++ src/main/java/com/yorkys/plugintest/config/ConfigManager.java
@@ -0,0 +1,14 @@
+package com.yorkys.plugintest.config;
+
+import com.yorkys.plugintest.MiniGame;
+import lombok.Getter;
+
+@Getter
+public class ConfigManager {
+    private MiniGame miniGame;
+    private final SettingsConfig settingsConfig;
+
+    public ConfigManager(MiniGame miniGame) {
+        this.settingsConfig = new SettingsConfig(config);
+    }
+}
