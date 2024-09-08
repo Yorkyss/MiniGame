26da06d67d8bcc0ee1df22e98bcf0148857db98c
Yorkys <yyorkyss@gmail.com>
2024-09-08 17:00:25 +0200

    Improved SettingsMenu and integrated improved TeamsManager system

--- src/main/java/com/yorkys/plugintest/Inventory/menus/SettingsMenu.java
+++ src/main/java/com/yorkys/plugintest/Inventory/menus/SettingsMenu.java
@@ -1,31 +1,46 @@
 package com.yorkys.plugintest.Inventory.menus;
 
-import com.yorkys.plugintest.Inventory.ClickableItem;
 import com.yorkys.plugintest.Inventory.Menu;
 import com.yorkys.plugintest.MiniGame;
-import com.yorkys.plugintest.utils.GlowEnchant;
+import com.yorkys.plugintest.players.MGPlayer;
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 
 import java.util.HashMap;
+import java.util.Objects;
 import java.util.stream.IntStream;
 
 public class SettingsMenu extends Menu {
     private MiniGame miniGame;
     private Player player;
+    private MGPlayer mgPlayer;
     private HashMap<Integer, Boolean> enchantedItems = new HashMap<>();
 
     public SettingsMenu(MiniGame miniGame, Player player) {
         super(miniGame, player, 9*3);
         this.miniGame = miniGame;
         this.player = player;
+        mgPlayer = miniGame.getTeamsManager().getMGPlayerFromPlayer(player);
     }
 
     @Override
     public void init() {
         IntStream.range(0, 9).forEach(i -> enchantedItems.put(i, true));
-        enchantedItems.put(12, false);
-        enchantedItems.put(14, false);
+
+        if (mgPlayer.getTeam() != null) {
+            if (Objects.equals(mgPlayer.getTeam().getColor(), "green")) {
+                enchantedItems.put(12, true);
+                enchantedItems.put(14, false);
+            } else if (Objects.equals(mgPlayer.getTeam().getColor(), "red")) {
+                enchantedItems.put(12, false);
+                enchantedItems.put(14, true);
+            }
+        } else {
+            enchantedItems.put(12, false);
+            enchantedItems.put(14, false);
+        }
+
         enchantedItems.put(26, false);
         super.customInit();
     }
@@ -34,21 +49,29 @@ public class SettingsMenu extends Menu {
     public void customInit() {
         IntStream.range(0, 9).forEach(i -> createItem(i, Material.STAINED_GLASS_PANE, 1, (byte) 7, "&7Settings", enchantedItems.get(i), null));
 
-        createItem(4, Material.REDSTONE_COMPARATOR, 1, "&aImpostazioni Aim Practice", "&7Modifica le impostazioni", "&7della tua sessione");
+        createItem(4, Material.REDSTONE_COMPARATOR, 1, "&aImpostazioni", "&7Scegli il colore", "&7del tuo team");
 
-        createItem(12, Material.WOOL, 1, "&bLANA", enchantedItems.get(12), event -> {
-            event.getWhoClicked().sendMessage("CLICCATA LANA");
-            enchantedItems.put(12, true);
-            enchantedItems.put(14, false);
-            customInit();
-        }, "&eClicca per LANA.");
+        createItem(12, Material.WOOL, 1, (byte) 5, "&aVERDE", enchantedItems.get(12), event -> {
+            if (miniGame.getTeamsManager().addGreenPlayer((Player) event.getWhoClicked())) {
+                enchantedItems.put(12, true);
+                enchantedItems.put(14, false);
+                event.getWhoClicked().sendMessage(ChatColor.AQUA + "Sei entrato nel team " + ChatColor.GREEN + "verde");
+                customInit();
+            } else {
+                event.getWhoClicked().sendMessage(ChatColor.RED + "Team pieno!");
+            }
+        }, "&eClicca per entrare nel team verde");
 
-        createItem(14, Material.STONE, 1, "&bPIETRA", enchantedItems.get(14), event -> {
-            event.getWhoClicked().sendMessage("CLICCATA PIETRA");
-            enchantedItems.put(12, false);
-            enchantedItems.put(14, true);
-            customInit();
-        }, "&eClicca per PIETRA.");
+        createItem(14, Material.WOOL, 1, (byte) 14, "&cROSSO", enchantedItems.get(14), event -> {
+            if (miniGame.getTeamsManager().addRedPlayer((Player) event.getWhoClicked())) {
+                enchantedItems.put(12, false);
+                enchantedItems.put(14, true);
+                event.getWhoClicked().sendMessage(ChatColor.AQUA + "Sei entrato nel team " + ChatColor.RED + "rosso");
+                customInit();
+            } else {
+                event.getWhoClicked().sendMessage(ChatColor.RED + "Team pieno!");
+            }
+        }, "&eClicca per entrare nel team rosso");
 
         createItem(26, Material.BARRIER, 1, "&4CHIUDI", enchantedItems.get(26), event -> {
             event.getWhoClicked().sendMessage("CLICCATA PIETRA");
