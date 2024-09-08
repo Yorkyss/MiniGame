46a791dd96f82a0ef8e72473f197fc1065a45a1c
Yorkys <yyorkyss@gmail.com>
2024-09-08 21:05:46 +0200

    Created SubCommand System for the MiniGame command (/mg)

--- src/main/java/com/yorkys/plugintest/commands/MiniGameCommand.java
+++ src/main/java/com/yorkys/plugintest/commands/MiniGameCommand.java
@@ -3,6 +3,8 @@ package com.yorkys.plugintest.commands;
 
 import com.google.common.collect.ImmutableList;
 import com.yorkys.plugintest.MiniGame;
+import com.yorkys.plugintest.commands.SubCommands.MiniGame.Start;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
@@ -11,32 +13,83 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
-@RequiredArgsConstructor
+
+@Getter
 public class MiniGameCommand implements CommandExecutor, TabCompleter {
 
+    private final Map<String, SubCommand> subCommands = new HashMap<>();
+
     private final MiniGame miniGame;
+    private CommandSender sender;
+
+    public MiniGameCommand(MiniGame miniGame) {
+        this.miniGame = miniGame;
+        SubCommand.mgCommand = this;
+
+        registerModule(new Start());
+    }
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (!(sender instanceof Player)) {
-            sender.sendMessage("Non sei un player?");
+        this.sender = sender;
+        if (args.length == 0) {
+            sender.sendMessage(ChatColor.RED + "Comando non valido");
             return true;
         }
-        Player player = ((Player) sender).getPlayer();
-
-        if (miniGame.getGameManager().start()) {
-            player.sendMessage(ChatColor.AQUA + "partita iniziata!");
-        } else {
-            player.sendMessage(ChatColor.RED + "impossibile iniziare la partita!!");
+        SubCommand subCommand = getByName(args[0]);
+        if (subCommand == null) {
+            sender.sendMessage(ChatColor.RED + "Comando non valido");
+            return true;
         }
-
+        if (!sender.hasPermission(subCommand.getPermission())) {
+            sender.sendMessage(ChatColor.RED + "Comando non valido");
+            return true;
+        }
+        String[] subCommandArgs;
+        if (args.length == 1) {
+            subCommandArgs = new String[0];
+        } else {
+            subCommandArgs = new String[args.length - 1];
+            System.arraycopy(args, 1, subCommandArgs, 0, args.length - 1);
+        }
+        try {
+            if (sender instanceof Player player &&
+                    subCommand.getClass() == subCommand.getClass().getMethod("execute", Player.class, String[].class).getDeclaringClass()) {
+                subCommand.execute(player, subCommandArgs);
+            } else {
+                subCommand.execute(sender, subCommandArgs);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return true;
     }
 
     @Override
-    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+    public List<String> onTabComplete(CommandSender sender, Command command, String label, String[] args) {
+        if (args.length == 1) {
+            return subCommands.keySet().stream()
+                    .filter(module -> module.startsWith(args[0].toLowerCase()))
+                    .collect(Collectors.toList());
+        } else if (args.length > 1) {
+            SubCommand subCommand = subCommands.get(args[0].toLowerCase());
+            if (subCommand != null) return subCommand.onTabComplete(sender, args);
+        }
         return ImmutableList.of();
+    }
+
+    private SubCommand getByName(String name) {
+        return subCommands.get(name.toLowerCase());
+    }
+
+    private void registerModule(SubCommand subCommand) {
+        subCommands.put(subCommand.getName().toLowerCase(), subCommand);
+        for (String alias : subCommand.getAliases())
+            subCommands.put(alias.toLowerCase(), subCommand);
     }
 }
---
+++ src/main/java/com/yorkys/plugintest/commands/SubCommand.java
@@ -0,0 +1,66 @@
+package com.yorkys.plugintest.commands;
+
+import com.google.common.collect.ImmutableList;
+import com.yorkys.plugintest.MiniGame;
+import com.yorkys.plugintest.utils.Colorizer;
+import lombok.Getter;
+import lombok.Setter;
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.util.StringUtil;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Getter
+public abstract class SubCommand {
+
+    protected static MiniGameCommand mgCommand;
+
+    protected final String name;
+    protected final String permission;
+    protected final String[] aliases;
+    @Setter
+    private String usage;
+
+    public SubCommand(String name, String permission, String... aliases) {
+        this.name = name;
+        this.permission = permission;
+        this.aliases = aliases;
+    }
+
+    public void execute(CommandSender sender, String[] args) {
+        sender.sendMessage(ChatColor.RED + "ERROR: SENDER IS NOT A PLAYER");
+    }
+    public void execute(Player player, String[] args) {}
+
+    public List<String> onTabComplete(CommandSender sender, String[] args) {
+        return ImmutableList.of();
+    }
+
+    protected List<String> filterArgs(String arg, String ...strings){
+        return StringUtil.copyPartialMatches(arg, Arrays.asList(strings), new ArrayList<>());
+    }
+
+    protected String builder(String[] strings) {
+        return builder(strings, 0);
+    }
+
+    protected String builder(String[] strings, int start) {
+        StringBuilder builder = new StringBuilder(strings[start]);
+        for (int i = start + 1; i < strings.length; i++) {
+            builder.append(" ").append(strings[i]);
+        }
+        return builder.toString();
+    }
+
+    public void sendMessage(String ...messages){
+        CommandSender sender = mgCommand.getSender();
+        if (sender == null) return;
+        for (String message : messages) {
+            sender.sendMessage(Colorizer.colorize(message));
+        }
+    }
+}
