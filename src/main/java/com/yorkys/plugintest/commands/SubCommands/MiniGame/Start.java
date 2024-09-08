ff23417eb61252a6c40d2c0b002e5bed283d9657
Yorkys <yyorkyss@gmail.com>
2024-09-08 21:06:05 +0200

    Added the Start SubCommand to MiniGame command

---
+++ src/main/java/com/yorkys/plugintest/commands/SubCommands/MiniGame/Start.java
@@ -0,0 +1,20 @@
+package com.yorkys.plugintest.commands.SubCommands.MiniGame;
+
+import com.yorkys.plugintest.commands.SubCommand;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+
+public class Start extends SubCommand {
+    public Start() {
+        super("start", "mg.admin");
+    }
+
+    @Override
+    public void execute(Player player, String[] args) {
+        if (mgCommand.getMiniGame().getGameManager().start()) {
+            player.sendMessage(ChatColor.AQUA + "partita iniziata!");
+        } else {
+            player.sendMessage(ChatColor.RED + "impossibile iniziare la partita!");
+        }
+    }
+}
