b3b82eb2359e93a7007004710b98e8c2a8ebfb3f
Yorkys <yyorkyss@gmail.com>
2024-09-08 22:05:21 +0200

    Added method to set type to MGPlayer class

--- src/main/java/com/yorkys/plugintest/players/MGPlayer.java
+++ src/main/java/com/yorkys/plugintest/players/MGPlayer.java
@@ -2,11 +2,9 @@ package com.yorkys.plugintest.players;
 
 import com.yorkys.plugintest.teams.Team;
 import lombok.Getter;
-import lombok.Setter;
 import org.bukkit.entity.Player;
 
 @Getter
-@Setter
 public class MGPlayer {
     private final Player player;
 
@@ -22,7 +20,22 @@ public class MGPlayer {
         this.team = team;
     }
 
-    public void removeTeam() { // use setTeam(null) to remove team from the player and player from the team
+    public void removeTeam() { // use setTeam(null) to remove team from the player and also player from the team
         team = null;
+    }
+
+    public boolean setType(PlayerType type) {
+        if (team == null) {
+            System.out.println("Error: can't set " + player.getName() + "'s type to " + type + " because the player doesn't have a team");
+            return false;
+        }
+
+        if (team.getPlayerFromType(type) != null) {
+            System.out.println("Error: can't set " + player.getName() + "'s type to " + type + " because this type is already taken");
+            return false;
+        }
+
+        this.type = type;
+        return true;
     }
 }
