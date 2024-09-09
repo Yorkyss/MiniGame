b66e64d9601eb0fbd133ce97189651909428a53e
Yorkys <yyorkyss@gmail.com>
2024-09-09 16:01:44 +0200

    Modified the NPCManager class to handle extended NPC classes

--- src/main/java/com/yorkys/plugintest/NPC/NPCManager.java
+++ src/main/java/com/yorkys/plugintest/NPC/NPCManager.java
@@ -2,28 +2,52 @@ package com.yorkys.plugintest.NPC;
 
 import com.yorkys.plugintest.MiniGame;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 public class NPCManager {
     private MiniGame miniGame;
 
     private List<NPC> npcs = new ArrayList<>();
+    private HashMap<UUID, String> npcsID = new HashMap<>();
 
     public NPCManager(MiniGame miniGame) {
         this.miniGame = miniGame;
     }
 
+    public NPC createNPC(NPC npc) {
+        npc.spawn();
+        addNPC(npc);
+        return npc;
+    }
+
     public void addNPC(NPC npc) {
         npcs.add(npc);
+        npcsID.put(npc.nmsEntity.getUniqueID(), npc.getID());
     }
 
     public void removeNPC(NPC npc) {
         npcs.remove(npc);
+        npcsID.remove(npc.nmsEntity.getUniqueID());
     }
 
-    public NPC getNPC(String name) {
-        return npcs.stream().filter(npc -> Objects.equals(npc.getName(), name)).findFirst().orElse(null);
+    public NPC getNPCFromCustomName(String customName) {
+        return npcs.stream().filter(npc -> Objects.equals(npc.getCustomName(), customName)).findFirst().orElse(null);
+    }
+
+    public NPC getNPCFromID(String ID) {
+        return npcs.stream().filter(npc -> Objects.equals(npc.getID(), ID)).findFirst().orElse(null);
+    }
+
+    public NPC getNPCFromID(UUID uuid) {
+        return getNPCFromID(npcsID.get(uuid));
+    }
+
+    public boolean existNPC(String customName) {
+        customName = customName.replace("§", "&");
+        return getNPCFromCustomName(customName) != null;
+    }
+
+    public boolean existNPC(UUID uuid) {
+        return getNPCFromID(uuid) != null;
     }
 }
