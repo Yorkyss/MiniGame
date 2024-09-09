a0099e40ff62b02f882fb8bd2e2d170e31a2bd58
Yorkys <yyorkyss@gmail.com>
2024-09-09 16:28:11 +0200

    Fixed SettingsConfig, now it store config values in variables hashmap to load once at plugin start

--- src/main/java/com/yorkys/plugintest/config/SettingsConfig.java
+++ src/main/java/com/yorkys/plugintest/config/SettingsConfig.java
@@ -1,21 +1,39 @@
 package com.yorkys.plugintest.config;
 
 import com.yorkys.plugintest.players.MGPlayer;
+import com.yorkys.plugintest.players.PlayerType;
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.configuration.file.FileConfiguration;
 
+import java.util.HashMap;
+import java.util.Objects;
+
 public class SettingsConfig {
     private final FileConfiguration config;
+    public HashMap<PlayerType, Location> greenPlayerTypeLocations = new HashMap<>();
+    public HashMap<PlayerType, Location> redPlayerTypeLocations = new HashMap<>();
 
     public SettingsConfig(FileConfiguration config) {
         this.config = config;
+        greenPlayerTypeLocations.put(PlayerType.TYPE1, getPlayerTypeLocation("player-spawn.green.type1"));
+        greenPlayerTypeLocations.put(PlayerType.TYPE2, getPlayerTypeLocation("player-spawn.green.type2"));
+        greenPlayerTypeLocations.put(PlayerType.TYPE3, getPlayerTypeLocation("player-spawn.green.type3"));
+        redPlayerTypeLocations.put(PlayerType.TYPE1, getPlayerTypeLocation("player-spawn.red.type1"));
+        redPlayerTypeLocations.put(PlayerType.TYPE2, getPlayerTypeLocation("player-spawn.red.type2"));
+        redPlayerTypeLocations.put(PlayerType.TYPE3, getPlayerTypeLocation("player-spawn.red.type3"));
+
     }
 
-    public Location getPlayerSpawnLocation(MGPlayer mgPlayer) {
-        if (mgPlayer.getType() == null) return null;
+    private Location getPlayerTypeLocation(String path) {
+        return new Location(Bukkit.getWorld("world"), config.getDouble(path + ".x"), config.getDouble(path + ".y"), config.getDouble(path + ".z"));
+    }
 
-        String path = "playersSpawn." + mgPlayer.getTeam().getColor() + "." + mgPlayer.getType().getName();
-        Location location = new Location(mgPlayer.getPlayer().getWorld(), config.getDouble(path + ".x"), config.getDouble(path + ".y"), config.getDouble(path + ".z"));
-        return location;
+    public Location getPlayerSpawnLocation(MGPlayer mgp) {
+        if (Objects.equals(mgp.getTeam().getColor(), "green")) {
+            return greenPlayerTypeLocations.get(mgp.getType());
+        } else {
+            return redPlayerTypeLocations.get(mgp.getType());
+        }
     }
 }
