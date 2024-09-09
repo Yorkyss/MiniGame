0a465f83253168dcd659ddd6e0a2f10bfeaa47cc
Yorkys <yyorkyss@gmail.com>
2024-09-09 16:01:36 +0200

    Created the main NPC class to allow the creation of extended NPC classes

--- src/main/java/com/yorkys/plugintest/NPC/NPC.java
+++ src/main/java/com/yorkys/plugintest/NPC/NPC.java
@@ -1,59 +1,64 @@
 package com.yorkys.plugintest.NPC;
 
-import com.mojang.authlib.GameProfile;
-import com.mojang.authlib.properties.Property;
-import net.minecraft.server.v1_8_R3.*;
-import org.bukkit.Bukkit;
+import net.minecraft.server.v1_8_R3.Entity;
+import net.minecraft.server.v1_8_R3.NBTTagCompound;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
-import org.bukkit.craftbukkit.v1_8_R3.CraftServer;
-import org.bukkit.craftbukkit.v1_8_R3.CraftWorld;
-import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
-import org.bukkit.entity.Player;
-
-import java.util.UUID;
+import org.bukkit.craftbukkit.v1_8_R3.entity.CraftEntity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Villager;
 
 public class NPC {
-    private Location location;
-    private String name;
-    private GameProfile gameProfile;
-    private EntityPlayer entityPlayer;
-    private String uuid;
-    private String texture;
-    private String signature;
+    protected String customName;
+    protected String ID;
+    protected Location location;
 
-    public NPC(Location location, String name, String uuid, String texture, String signature) {
+    protected Entity nmsEntity;
+    protected boolean hasAI;
+
+
+    public NPC(String customName, String ID, Location location, boolean hasAI) {
+        this.customName = customName;
+        this.ID = ID;
         this.location = location;
-        this.name = name;
-        this.uuid = uuid;
-        this.texture = texture;
-        this.signature = signature;
+        this.hasAI = hasAI;
     }
 
     public void spawn() {
-        MinecraftServer minecraftServer = ((CraftServer) Bukkit.getServer()).getServer();
-        WorldServer worldServer = ((CraftWorld) location.getWorld()).getHandle();
+        Villager villager = (Villager) location.getWorld().spawnEntity(location, EntityType.VILLAGER);
 
-        gameProfile = new GameProfile(UUID.fromString(uuid), ChatColor.translateAlternateColorCodes('&', name)) ;
-        gameProfile.getProperties().put("textures", new Property("textures", texture, signature));
+        nmsEntity = ((CraftEntity) villager).getHandle();
+        setAI(hasAI());
 
-        entityPlayer = new EntityPlayer(minecraftServer, worldServer, gameProfile, new PlayerInteractManager(worldServer));
-        entityPlayer.setLocation(location.getX(), location.getY(), location.getZ(), location.getYaw(), location.getPitch());
+        villager.setCustomName(ChatColor.translateAlternateColorCodes('&', customName));
+        villager.setCustomNameVisible(true);
     }
 
-    public void show(Player player) {
-        PlayerConnection playerConnection = ((CraftPlayer) player).getHandle().playerConnection;
+    public void setAI(boolean value) {
+        int ai = value ? 0 : 1;
 
-        playerConnection.sendPacket(new PacketPlayOutPlayerInfo(PacketPlayOutPlayerInfo.EnumPlayerInfoAction.ADD_PLAYER, entityPlayer));
-        playerConnection.sendPacket(new PacketPlayOutNamedEntitySpawn(entityPlayer));
-        playerConnection.sendPacket(new PacketPlayOutEntityHeadRotation(entityPlayer,(byte) (entityPlayer.yaw * 256 / 300)));
+        NBTTagCompound tag = nmsEntity.getNBTTag();
+        if(tag == null) {
+            tag = new NBTTagCompound();
+        }
+        nmsEntity.c(tag);
+        tag.setInt("NoAI", ai);
+        nmsEntity.f(tag);
     }
 
-    public String getName() {
-        return name;
+    public String getCustomName() {
+        return customName;
     }
 
-    public EntityPlayer getEntityPlayer() {
-        return entityPlayer;
+    public String getID() {
+        return ID;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public boolean hasAI() {
+        return hasAI;
     }
 }
