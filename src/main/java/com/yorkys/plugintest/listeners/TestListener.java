ecb1677d91c36452d7eea99190d8615f2ce61be0
Yorkys <yyorkyss@gmail.com>
2024-09-10 15:42:52 +0200

    Added a method to prevent the player from hitting the shop NPC

--- src/main/java/com/yorkys/plugintest/listeners/TestListener.java
+++ src/main/java/com/yorkys/plugintest/listeners/TestListener.java
@@ -5,9 +5,11 @@ import lombok.RequiredArgsConstructor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 
 import java.util.Objects;
+import java.util.UUID;
 
 @RequiredArgsConstructor
 public class TestListener implements Listener {
@@ -15,15 +17,24 @@ public class TestListener implements Listener {
     private final MiniGame miniGame;
 
     @EventHandler
-    public void onEntityInteract(PlayerInteractEntityEvent e) {
-        Player player = e.getPlayer();
-        if (!miniGame.getNpcManager().existNPC(e.getRightClicked().getUniqueId())) return;
+    public void onEntityInteract(PlayerInteractEntityEvent event) {
+        Player player = event.getPlayer();
+        if (!miniGame.getNpcManager().existNPC(event.getRightClicked().getUniqueId())) return;
 
-        if (Objects.equals(miniGame.getNpcManager().getNPCFromID(e.getRightClicked().getUniqueId()).getID(), "shop")) {
-            e.setCancelled(true);
+        if (Objects.equals(miniGame.getNpcManager().getNPCFromID(event.getRightClicked().getUniqueId()).getID(), "shop")) {
+            event.setCancelled(true);
             player.sendMessage("&btest");
         }
 
+    }
+
+    @EventHandler
+    public void onEntityDamage(EntityDamageEvent event) {
+        UUID entityUUID = event.getEntity().getUniqueId();
+
+        if (miniGame.getNpcManager().existNPC(entityUUID) && Objects.equals(miniGame.getNpcManager().getNPCFromID(entityUUID).getID(), "shop")) {
+            event.setCancelled(true);
+        }
     }
 }
 
