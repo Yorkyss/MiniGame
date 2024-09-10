ccca810bd472ff8fe283625bf5f7f7d6555d7dfe
Yorkys <yyorkyss@gmail.com>
2024-09-10 16:50:31 +0200

    Implemented getting npc shop values from SettingsConfig

--- src/main/java/com/yorkys/plugintest/NPC/NPCManager.java
+++ src/main/java/com/yorkys/plugintest/NPC/NPCManager.java
@@ -7,35 +7,45 @@ import java.util.*;
 public class NPCManager {
     private MiniGame miniGame;
 
-    private List<NPC> npcs = new ArrayList<>();
+    private List<NPC> spawnedNPCs = new ArrayList<>();
     private HashMap<UUID, String> npcsID = new HashMap<>();
 
     public NPCManager(MiniGame miniGame) {
         this.miniGame = miniGame;
     }
 
-    public NPC createNPC(NPC npc) {
+    private void addNPC(NPC npc) {
+        spawnedNPCs.add(npc);
+        npcsID.put(npc.getUUID(), npc.getID());
+    }
+
+    private void removeNPC(NPC npc) {
+        spawnedNPCs.remove(npc);
+        npcsID.remove(npc.getUUID());
+        npc.removeNPC();
+    }
+
+    private void spawnNPC(NPC npc) {
         npc.spawn();
         addNPC(npc);
-        return npc;
     }
 
-    public void addNPC(NPC npc) {
-        npcs.add(npc);
-        npcsID.put(npc.nmsEntity.getUniqueID(), npc.getID());
+    public void spawnAllNPCs() {
+        miniGame.getConfigManager().getSettingsConfig().getShopNPCs().forEach(this::spawnNPC);
     }
 
-    public void removeNPC(NPC npc) {
-        npcs.remove(npc);
-        npcsID.remove(npc.nmsEntity.getUniqueID());
+    public void removeAllNPCs() {
+        System.out.println(spawnedNPCs);
+        spawnedNPCs.forEach(NPC::removeNPC);
     }
+
 
     public NPC getNPCFromCustomName(String customName) {
-        return npcs.stream().filter(npc -> Objects.equals(npc.getCustomName(), customName)).findFirst().orElse(null);
+        return spawnedNPCs.stream().filter(npc -> Objects.equals(npc.getCustomName(), customName)).findFirst().orElse(null);
     }
 
     public NPC getNPCFromID(String ID) {
-        return npcs.stream().filter(npc -> Objects.equals(npc.getID(), ID)).findFirst().orElse(null);
+        return spawnedNPCs.stream().filter(npc -> Objects.equals(npc.getID(), ID)).findFirst().orElse(null);
     }
 
     public NPC getNPCFromID(UUID uuid) {
