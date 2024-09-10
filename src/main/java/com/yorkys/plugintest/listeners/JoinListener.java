237637eecbbfd6fc3527d791e7296f9cf236da75
Yorkys <yyorkyss@gmail.com>
2024-09-10 21:21:47 +0200

    Removed debugging SOUTs from JoinListener

--- src/main/java/com/yorkys/plugintest/listeners/JoinListener.java
+++ src/main/java/com/yorkys/plugintest/listeners/JoinListener.java
@@ -2,6 +2,7 @@ package com.yorkys.plugintest.listeners;
 
 import com.yorkys.plugintest.MiniGame;
 import lombok.RequiredArgsConstructor;
+import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -15,20 +16,22 @@ public class JoinListener implements Listener {
 
     @EventHandler
     public void onJoin(PlayerJoinEvent event) {
-        event.setJoinMessage("+ " + event.getPlayer().getName());
+        event.setJoinMessage(ChatColor.GREEN + "+" + ChatColor.GRAY + event.getPlayer().getName());
         miniGame.getTeamsManager().addPlayerToQueue(event.getPlayer());
 
-        System.out.println("Player in queue:");
-        miniGame.getTeamsManager().getMgPlayers().forEach(p -> System.out.println(p.getPlayer().getName()));
+//        DEBUGGING QUEUE JOIN
+//        System.out.println("Player in queue:");
+//        miniGame.getTeamsManager().getMgPlayers().forEach(p -> System.out.println(p.getPlayer().getName()));
     }
 
     @EventHandler
     public void onQuit(PlayerQuitEvent event) {
-        event.setQuitMessage("- " + event.getPlayer().getName());
+        event.setQuitMessage(ChatColor.RED + "+" + ChatColor.GRAY + event.getPlayer().getName());
         miniGame.getTeamsManager().removePlayerToQueue(event.getPlayer());
 
-        System.out.println("Player in queue:");
-        miniGame.getTeamsManager().getMgPlayers().forEach(p -> System.out.println(p.getPlayer().getName()));
+//        DEBUGGING QUEUE QUIT
+//        System.out.println("Player in queue:");
+//        miniGame.getTeamsManager().getMgPlayers().forEach(p -> System.out.println(p.getPlayer().getName()));
     }
 
     @EventHandler
