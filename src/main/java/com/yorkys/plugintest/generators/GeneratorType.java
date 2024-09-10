7f11a9e7c935f73693dc8b4e2643bf46fa6082e0
Yorkys <yyorkyss@gmail.com>
2024-09-10 03:40:49 +0200

    Modified GeneratorType for the new Generator System, now every type have just a name and there is a method to get the GeneratorType.type from the name (es: "iron", "gold" etc..)

--- src/main/java/com/yorkys/plugintest/generators/GeneratorType.java
+++ src/main/java/com/yorkys/plugintest/generators/GeneratorType.java
@@ -1,42 +1,24 @@
 package com.yorkys.plugintest.generators;
 
-import org.bukkit.Material;
-
 public enum GeneratorType {
-    IRON(Material.IRON_BLOCK, Material.IRON_INGOT, 2, -1, 12),
-    GOLD(Material.GOLD_BLOCK, Material.GOLD_INGOT, 3, -1, 4),
-    DIAMOND(Material.DIAMOND_BLOCK, Material.DIAMOND, 5, 30, 2);
-    private final Material blockMaterial;
-    private final Material itemMaterial;
-    private final int spawnTime;
-    private final int upgradeTime;
-    private final int maxStackSize;
+    IRON("iron"),
+    GOLD("gold"),
+    DIAMOND("diamond");
 
-    GeneratorType(Material blockMaterial, Material itemMaterial, int spawnTime, int upgradeTime, int maxStackSize) {
-        this.blockMaterial = blockMaterial;
-        this.itemMaterial = itemMaterial;
-        this.spawnTime = spawnTime;
-        this.upgradeTime = upgradeTime;
-        this.maxStackSize = maxStackSize;
+    private final String name;
+
+    GeneratorType(String name) {
+        this.name = name;
     }
 
-    public Material getBlockMaterial() {
-        return blockMaterial;
-    }
+    public String getName() { return name; }
 
-    public Material getItemMaterial() {
-        return itemMaterial;
-    }
-
-    public int getSpawnTime() {
-        return spawnTime;
-    }
-
-    public int getUpgradeTime() {
-        return upgradeTime;
-    }
-
-    public int getMaxStackSize() {
-        return maxStackSize;
+    public static GeneratorType fromName(String name) {
+        for (GeneratorType type : GeneratorType.values()) {
+            if (type.getName().equalsIgnoreCase(name)) {
+                return type;
+            }
+        }
+        throw new IllegalArgumentException("No enum constant with name: " + name);
     }
 }
