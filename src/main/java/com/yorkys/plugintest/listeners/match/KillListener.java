5b4b6f32ca93a375355fe722b4dbb2e463e84f16
Yorkys <yyorkyss@gmail.com>
2024-09-11 15:58:19 +0200

    Implemented Team and MGPlayer new methods to assign stars in KillListener

--- src/main/java/com/yorkys/plugintest/listeners/match/KillListener.java
+++ src/main/java/com/yorkys/plugintest/listeners/match/KillListener.java
@@ -1,6 +1,7 @@
 package com.yorkys.plugintest.listeners.match;
 
 import com.yorkys.plugintest.MiniGame;
+import com.yorkys.plugintest.players.MGPlayer;
 import lombok.RequiredArgsConstructor;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
@@ -16,13 +17,41 @@ public class KillListener implements Listener {
     @EventHandler
     public void onEntityInteract(PlayerDeathEvent event) {
         Player deadPlayer = event.getEntity();
+        MGPlayer deadPlayerMGP = miniGame.getTeamsManager().getMGPlayerFromPlayer(deadPlayer);
+
         if (deadPlayer.getKiller() == null) {
             event.setDeathMessage(ChatColor.GRAY + deadPlayer.getName() + " è morto");
+            deadPlayerMGP.resetStars();
+            deadPlayer.sendMessage( "hai perso le tue stelle rosse!");
             return;
         }
 
         Player killer = event.getEntity().getKiller();
-        killer.sendMessage("+1");
+        MGPlayer killerMGP = miniGame.getTeamsManager().getMGPlayerFromPlayer(killer);
+
+
+        int stars = deadPlayerMGP.getStars() < 1 ? 1 : deadPlayerMGP.getStars() * 2;
+        killerMGP.updateStars(stars);
+        deadPlayerMGP.resetStars();
+
+
+        if (killerMGP.getTeam().getRedStars() >= 30) {
+            killerMGP.getTeam().addBlueStar(1);
+            killerMGP.getTeam().resetRedStars();
+
+            deadPlayerMGP.getTeam().resetRedStars();
+        }
+
+        event.setDeathMessage(ChatColor.AQUA + deadPlayer.getName() + ChatColor.GRAY + " è stato ucciso da " + ChatColor.AQUA + killer.getName());
+
+        killer.sendMessage(ChatColor.GRAY + "+ " + ChatColor.RED + stars + ChatColor.GRAY + " stelle rosse");
+        killer.sendMessage(killerMGP.getStars() + " tue stelle rosse");
+        killer.sendMessage(killerMGP.getTeam().getRedStars() + " team stelle rosse");
+        killer.sendMessage(killerMGP.getTeam().getBlueStars() + " team stelle blue");
+
+        deadPlayer.sendMessage( "hai perso le tue stelle rosse!");
+
+
 
     }
 }
