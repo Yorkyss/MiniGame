c562f05b85d8918c8d6cc89cf94fb55af31a0094
Yorkys <yyorkyss@gmail.com>
2024-09-13 01:40:30 +0200

    Implemented new death and kills variables of MGPlayer in KillListener

--- src/main/java/com/yorkys/plugintest/listeners/match/KillListener.java
+++ src/main/java/com/yorkys/plugintest/listeners/match/KillListener.java
@@ -1,8 +1,11 @@
 package com.yorkys.plugintest.listeners.match;
 
 import com.yorkys.plugintest.MiniGame;
+import com.yorkys.plugintest.gameManager.GameRounds;
+import com.yorkys.plugintest.gameManager.GameStates;
 import com.yorkys.plugintest.players.MGPlayer;
 import lombok.RequiredArgsConstructor;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -16,43 +19,47 @@ public class KillListener implements Listener {
 
     @EventHandler
     public void onEntityInteract(PlayerDeathEvent event) {
+        // SETTING DEAD STARS
         Player deadPlayer = event.getEntity();
         MGPlayer deadPlayerMGP = miniGame.getMgPlayersManager().getMGPlayerFromPlayer(deadPlayer);
 
-        if (deadPlayer.getKiller() == null) {
-            event.setDeathMessage(ChatColor.GRAY + deadPlayer.getName() + " è morto");
-            deadPlayerMGP.resetStars();
-            deadPlayer.sendMessage( "hai perso le tue stelle rosse!");
+        if (miniGame.getGameManager().getGameState() != GameStates.PLAYING || deadPlayerMGP.isSpectator()) {
             return;
         }
 
+        // CHECK IF DEAD PLAYER WERE KILLED BY ANOTHER PLAYER
+        if (deadPlayer.getKiller() == null) {
+            event.setDeathMessage(ChatColor.YELLOW + "" + deadPlayer.getName() + ChatColor.GRAY + " è morto perdendo " + ChatColor.RED + deadPlayerMGP.getStars() + ChatColor.GRAY + " stelle rosse");
+            deadPlayerMGP.resetStars();
+            return;
+        }
+        deadPlayerMGP.resetStars();
+
+        // SETTING KILLER STARS
         Player killer = event.getEntity().getKiller();
         MGPlayer killerMGP = miniGame.getMgPlayersManager().getMGPlayerFromPlayer(killer);
 
-
-        int stars = deadPlayerMGP.getStars() < 1 ? 1 : deadPlayerMGP.getStars() * 2;
+        int stars = deadPlayerMGP.getStars() < 1 ? 29 : deadPlayerMGP.getStars() * 29;
         killerMGP.updateStars(stars);
-        deadPlayerMGP.resetStars();
+        String starsWord = stars == 1 ? " stella " : " stelle ";
+        event.setDeathMessage(ChatColor.AQUA + killer.getName() + ChatColor.GRAY + " ha rubato " + stars + starsWord + "rosse uccidendo " + ChatColor.YELLOW + deadPlayer.getName());
 
 
+        // BLUE STARS AND FINISH GAME CHECK
         if (killerMGP.getTeam().getRedStars() >= 30) {
             killerMGP.getTeam().addBlueStar(1);
             killerMGP.getTeam().resetRedStars();
-
             deadPlayerMGP.getTeam().resetRedStars();
+            Bukkit.getServer().broadcastMessage(ChatColor.GRAY + "il team " + ChatColor.BLUE + killerMGP.getTeam().getColor() + ChatColor.GRAY + " ha ottenuto 1 stella blu!");
+            Bukkit.getServer().broadcastMessage(ChatColor.GRAY + "stelle rosse resettate");
+
+            if (GameRounds.fromRound(miniGame.getGameManager().getGameRounds().getRound() + 1) != null) {
+                miniGame.getGameManager().setGameRounds(GameRounds.fromRound(miniGame.getGameManager().getGameRounds().getRound() + 1));
+            } else {
+                Bukkit.getServer().broadcastMessage(ChatColor.GRAY + "il team " + ChatColor.AQUA + killerMGP.getTeam().getColor() + ChatColor.GRAY + " ha vinto la partita!");
+                miniGame.getGameManager().stop();
+            }
         }
-
-        event.setDeathMessage(ChatColor.AQUA + deadPlayer.getName() + ChatColor.GRAY + " è stato ucciso da " + ChatColor.AQUA + killer.getName());
-
-        killer.sendMessage(ChatColor.GRAY + "+ " + ChatColor.RED + stars + ChatColor.GRAY + " stelle rosse");
-        killer.sendMessage(killerMGP.getStars() + " tue stelle rosse");
-        killer.sendMessage(killerMGP.getTeam().getRedStars() + " team stelle rosse");
-        killer.sendMessage(killerMGP.getTeam().getBlueStars() + " team stelle blue");
-
-        deadPlayer.sendMessage( "hai perso le tue stelle rosse!");
-
-
-
     }
 }
 
