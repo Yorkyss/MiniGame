28c10d228488540aaccc744ddf8580b44e2a6ff3
Yorkys <yyorkyss@gmail.com>
2024-09-12 22:24:27 +0200

    Added MGPlayersManager to manage MGPlayers, now all players will have their own MGPlayer object and not just in-game/queue players

---
+++ src/main/java/com/yorkys/plugintest/players/MGPlayersManager.java
@@ -0,0 +1,44 @@
+package com.yorkys.plugintest.players;
+
+import com.yorkys.plugintest.MiniGame;
+import com.yorkys.plugintest.gameManager.GameStates;
+import org.bukkit.entity.Player;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MGPlayersManager {
+    private final MiniGame miniGame;
+    private List<MGPlayer> mgPlayers = new ArrayList<>();
+
+    public MGPlayersManager(MiniGame miniGame) {
+        this.miniGame = miniGame;
+    }
+
+    public void addPlayer(Player player) {
+        mgPlayers.add(new MGPlayer(player));
+    }
+
+    public void addPlayer(MGPlayer player) {
+        mgPlayers.add(player);
+    }
+
+    public boolean removePlayer(Player player) {
+            MGPlayer mgp = getMGPlayerFromPlayer(player);
+            if (mgp == null) return true;
+            mgp.setTeam(null);
+            mgPlayers.remove(getMGPlayerFromPlayer(player));
+            return true;
+    }
+
+    public MGPlayer getMGPlayerFromPlayer(Player player) {
+        return mgPlayers.stream()
+                .filter(p -> p.getPlayer().getUniqueId().equals(player.getUniqueId()))
+                .findFirst()
+                .orElse(null);
+    }
+
+    public List<MGPlayer> getMgPlayers() {
+        return  mgPlayers;
+    }
+}
