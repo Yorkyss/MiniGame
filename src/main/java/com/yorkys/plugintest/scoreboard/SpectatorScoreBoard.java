192d0e0c4585200ac9db8c0ad3b314c011fea72e
Yorkys <yyorkyss@gmail.com>
2024-09-12 22:26:09 +0200

    Added a SpectatorScoreBoard

---
+++ src/main/java/com/yorkys/plugintest/scoreboard/SpectatorScoreBoard.java
@@ -0,0 +1,14 @@
+package com.yorkys.plugintest.scoreboard;
+
+import com.yorkys.plugintest.MiniGame;
+import org.bukkit.entity.Player;
+
+public class SpectatorScoreBoard {
+    public static void createNewScoreboard(Player player, MiniGame miniGame) {
+        System.out.println("CREATING BUT SPECT");
+    }
+
+    public static void updateScoreBoard(Player player, MiniGame miniGame) {
+        System.out.println("UPDATING BUT SPECT");
+    }
+}
