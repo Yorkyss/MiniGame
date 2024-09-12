f74b6a250f7939f95a741940a1908dd1a0125ffa
Yorkys <yyorkyss@gmail.com>
2024-09-12 19:25:15 +0200

    Added ScoreBoardManager to handle scoreboard (queue and in game)

---
+++ src/main/java/com/yorkys/plugintest/scoreboard/ScoreBoardManager.java
@@ -0,0 +1,52 @@
+package com.yorkys.plugintest.scoreboard;
+
+import com.yorkys.plugintest.MiniGame;
+import com.yorkys.plugintest.gameManager.GameStates;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scoreboard.*;
+
+public final class ScoreBoardManager {
+
+    private final MiniGame miniGame;
+    private BukkitRunnable task;
+
+    public ScoreBoardManager(MiniGame miniGame) {
+        this.miniGame = miniGame;
+        startTask();
+    }
+
+    public void startTask() {
+        miniGame.runTaskTimer(task = new BukkitRunnable() {
+            @Override
+            public void run() {
+                for (Player player : Bukkit.getOnlinePlayers()) {
+                    if (player.getScoreboard() != null && player.getScoreboard().getObjective("MiniGame") != null)
+                        updateScoreboard(player);
+                    else
+                        createNewScoreboard(player);
+                }
+            }
+        }, 0, 20);
+    }
+
+    public void cancelTask() {
+        if (task != null) {
+            task.cancel();
+        }
+    }
+
+    public void createNewScoreboard(Player player) {
+        if (miniGame.getGameManager().getGameState() == GameStates.QUEUE) {
+            QueueScoreBoard.createNewScoreboard(player, miniGame);
+        }
+    }
+
+    public void updateScoreboard(Player player) {
+        if (miniGame.getGameManager().getGameState() == GameStates.QUEUE) {
+            QueueScoreBoard.updateScoreBoard(player, miniGame);
+        }
+    }
+}
